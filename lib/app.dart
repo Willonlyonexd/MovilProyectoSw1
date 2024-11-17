0fee9d2de114da02f208eac8d25c555c490c8a2e
RafaelSvgh <salvatierravallerafael@gmail.com>
2024-11-17 00:57:14 -0400

    merge con wilson

--- lib/app.dart
+++ lib/app.dart
@@ -1,13 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:reproductor_colaborativo_sw1/views/screens/create_room_screen.dart';
-import 'package:reproductor_colaborativo_sw1/views/screens/join_room_screen.dart';
-import 'package:reproductor_colaborativo_sw1/views/screens/login_screen.dart';
-import 'package:reproductor_colaborativo_sw1/views/screens/home_screen.dart'; // Pantalla principal
-import 'package:reproductor_colaborativo_sw1/views/screens/principal_room_screen.dart'; // Sala principal
 import 'package:reproductor_colaborativo_sw1/src/views/screens/create_room_screen.dart';
 import 'package:reproductor_colaborativo_sw1/src/views/screens/join_room_screen.dart';
-import 'src/views/screens/login_screen.dart';
-import 'src/views/screens/home_screen.dart'; // Importa la pantalla principal
+import 'package:reproductor_colaborativo_sw1/src/views/screens/login_screen.dart';
+import 'package:reproductor_colaborativo_sw1/src/views/screens/home_screen.dart'; // Pantalla principal
+import 'package:reproductor_colaborativo_sw1/src/views/screens/principal_room_screen.dart'; // Sala principal
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -26,12 +22,8 @@ class MyApp extends StatelessWidget {
         '/home': (context) => const HomeScreen(), // Pantalla principal.
         '/create_room': (context) => const CreateRoomScreen(), // Crear sala.
         '/join_room': (context) => const JoinRoomScreen(), // Unirse a una sala.
-        '/principal_room': (context) => const PrincipalRoomScreen(), // Unirse a una sala.
-
-        '/create_room': (context) =>
-            const CreateRoomScreen(), // Pantalla de inicio de sesión.
-        '/join_room': (context) =>
-            const JoinRoomScreen(), // Pantalla principal.
+        '/principal_room': (context) =>
+            const PrincipalRoomScreen(), // Unirse a una sala.
       },
     );
   }
